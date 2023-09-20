6223cd219a9a2b50a969bb97902aedc775fe3aee
Yeimmy Lopez <yeimmy.lopez@iabell.com>
2023-09-20 18:31:59 -0500

    create random number and condicionals for the user

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -4,16 +4,29 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static  void  main(String[] args) {
-        System.out.println("Bienvenid@ a Guess the Number, donde debes de adivinar un número de 1 a 100");
+        System.out.println("¡Bienvenid@ a Guess the Number!, dondé debes de adivinar un número de 1 a 100 donde contaras con 5 intentos. ¡Suerte! ☺");
 
-        // Entrada de datos
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa un número");
-        String numberUser = sc.nextLine();
-        if (numberUser == "100") System.out.println("El número" + numberUser + "es 100");
-        else {
-         System.out.println("El número no es 100");
+        int randomNumber = (int)Math.floor(Math.random() * (100 - 1 + 1) + 1);
+        System.out.println(randomNumber);
+
+
+        for (int i = 0; i < 5; i++) {
+            // Entrada de datos
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Ingresa un número: ");
+
+            // Guardar número del jugador
+            int userNumber = sc.nextInt();
+
+            if (userNumber > randomNumber){
+                System.out.println("Oh, tu número es muy alto :(");
+            } else if (userNumber < randomNumber) {
+                System.out.println("Oh, tu número es muy bajo :(");
+            }  else {
+                System.out.println("Haz acertado el número! ☻");
+            }
+
         }
-        }
+    }
     }
 
