e11110c898525e8fc601dae97995a056175a7945
Yeimmy Lopez <108906765+VanesaLo@users.noreply.github.com>
2023-09-25 16:51:51 -0500

    add computer like a player

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -3,30 +3,51 @@ package src;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-    public static  void  main(String[] args) {
-        System.out.println("¡Bienvenid@ a Guess the Number!, dondé debes de adivinar un número de 1 a 100 donde contaras con 5 intentos. ¡Suerte! ☺");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("¡Bienvenid@ a Guess the Number!" +
+                " donde debes adivinar un número de 1 a 100 y cuentas con 5 intentos." +
+                " ¡Suerte! 😊");
 
-        int randomNumber = (int)Math.floor(Math.random() * (100 - 1 + 1) + 1);
+        int randomNumber = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
         System.out.println(randomNumber);
 
+        int tries = 0;
 
-        for (int i = 0; i < 5; i++) {
-            // Entrada de datos
+
+        while (tries < 5) {
+            // Entrada de datos del jugador
             Scanner sc = new Scanner(System.in);
-            System.out.print("Ingresa un número: ");
+            System.out.println("---------------------------Ingresa un número--------------------------- ");
 
             // Guardar número del jugador
             int userNumber = sc.nextInt();
 
-            if (userNumber > randomNumber){
-                System.out.println("Oh, tu número es muy alto :(");
+            if (userNumber > randomNumber) {
+                System.out.println("Oh, tu número es muy alto ☹️");
             } else if (userNumber < randomNumber) {
-                System.out.println("Oh, tu número es muy bajo :(");
-            }  else {
-                System.out.println("Haz acertado el número! ☻");
+                System.out.println("Oh, tu número es muy bajo ☹️");
+            } else {
+                System.out.println("Haz acertado el número! 🤗");
             }
 
+            // Turno de la Robbie la maquina
+            System.out.println("---------------------------Es el turno de Robbie---------------------------");
+            int computerNumber = (int) Math.floor(Math.random() * 100 + 1);
+            Thread.sleep(500);
+            System.out.println("Robbie eligio: " + computerNumber);
+
+            if (computerNumber > randomNumber) {
+                System.out.println("Oh, el número es muy alto ☹️");
+            } else if (computerNumber < randomNumber) {
+                System.out.println("Oh, el número es muy bajo ☹️");
+            } else {
+                System.out.println("¡Robbie ha acertado el número! 🤗");
+                break;
+            }
+
+            tries++;
         }
-    }
+
     }
 
+}
